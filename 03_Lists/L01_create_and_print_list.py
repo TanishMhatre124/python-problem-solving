b15d76563d6827f379b60fd103b7794f9dc3dee0
Tanish Mhatre <mhatretanish417@gmail.com>
2026-07-22 17:17:02 +0530

    Start Lists Level 1 (L01-L02): List creation and basic operations

---
+++ 03_Lists/L01_create_and_print_list.py
@@ -0,0 +1,22 @@
+# ==========================================================
+# Question L01: Create and Print a List
+# ----------------------------------------------------------
+# Write a Python program that creates a list containing
+# five favorite fruits.
+#
+# Display:
+#
+# • Complete List
+#
+# Example Output:
+#
+# Fruits:
+# Apple
+# Mango
+# Banana
+# Orange
+# Grapes
+# ==========================================================
+
+fruits=["apple","mango","banana","orange","grapes"]
+print(fruits)
